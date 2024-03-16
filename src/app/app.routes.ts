70ce366bbf54a233db2726c6888b007319a15d18
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-16 18:31:59 -0500

    create route home, customer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'customer', component: CustomerComponent}
+];
