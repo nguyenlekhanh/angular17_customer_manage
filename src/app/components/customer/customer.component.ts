cb03636c3f3ddc026c22843990be3cfa6e515138
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-16 22:48:05 -0500

    show customer data in material table

--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { MasterService } from '../../_services/master.service';
 import { Posts } from '../../../_model/posts';
 import { Customer } from '../../../_model/Customer';
+import { MaterialModule } from '../../../_module/Material.Module';
+import { MatTableDataSource } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
@@ -14,6 +20,9 @@ export class CustomerComponent {
   constructor(private service: MasterService){}
 
   customerData!: Customer[];
+
+  datasource: any;
+  displayColumns: string[]=['code', 'name', 'email', 'password']
 
   ngOnInit() {
     this.LoadInitialData();
@@ -23,6 +32,7 @@ export class CustomerComponent {
     this.service.GetAllCustomer().subscribe((item) => {
       this.customerData = item;
       console.log(this.customerData);
+      this.datasource = new MatTableDataSource(this.customerData);
     })
   }
 }
