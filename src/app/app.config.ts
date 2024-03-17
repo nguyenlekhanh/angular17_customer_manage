e2d1dc4aca5b3d294de5d018a4cf3a29cd7cab04
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-16 22:50:12 -0500

    install ngrx store and effect

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,15 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './_services/token.interceptor';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([tokenInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideStore(),
+    provideEffects()
+]
 };
